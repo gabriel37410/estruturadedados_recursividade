cfffb31443ca825ef07145fcfda20b34956c49a8
gabriel37410 <gabriel_37410@yahoo.com>
2020-03-31 23:10:17 -0300

    Enunciado adicionado

--- Main.c
+++ Main.c
@@ -10,3 +10,20 @@ int main() {
     exercicio3();
     exercicio4();
 }
+
+/* ENUNCIADOS
+1. Implemente uma função recursiva que, dados dois números inteiros x e n, 
+calcula o valor de x elevado na potencia de n.
+
+2. Crie uma função recursiva que , dados dois números inteiros x e n, resolva a 
+multiplicação dos números através de somas sucessivas. Por exemplo, 3 e 7 -> 7 + 7 + 7 = 21 
+(somar três vezes o número sete).
+
+3. Escreva uma função recursiva que determine quantas vezes um dígito K ocorre em um 
+número natural N. Por exemplo, o dígito 2 ocorre 3 vezes em 762021192. O valor pode ser digitado como String.
+
+4. Resolver o problema da Torre de Hanoi que esta no material.
+
+Essa atividade contará como presença deve ser enviado o link do github de onde se encontram as respostas. 
+Pode ser feito em dupla.
+*/
